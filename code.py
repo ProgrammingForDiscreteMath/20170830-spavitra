538ba76da5fb10ab62fa5345674004e3b7e66090
spavitra <31251474+spavitra@users.noreply.github.com>
2017-09-06 16:49:19 +0530

    Add files via upload

--- code.py
+++ code.py
@@ -5,10 +5,11 @@ def fourth_element_of_list(L):
     """
     Return the nth element of ``L`` if it exists, ``None`` otherwise.
     """
-    if len(L) < i:
+    try:
+        return L[3]
+    except:
         return None
-    else:
-        return L[i]
+
 
 # 2
 # Modify to use try-except to return the sum of all numbers in L,
@@ -18,12 +19,16 @@ def sum_of_numbers(L):
     """
     Return the sum of all the numbers in L.
     """
-    s = 0
-    for l in L:
-        if type(l) is int or type(l) is float:
-            s += l
-    return s
+    
+    h=0
+    for i in L:
+        try:
+            h+=i
+        except:
+            h+=0
+    return h
+
+
 
 # TEST
 # sum_of_numbers([3, 1.9, 's']) == 4.9
-
